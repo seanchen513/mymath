a582ccd7a06cff19f232910b97d5899bae59f320
Sean Chen <32373135+seanchen513@users.noreply.github.com>
2019-02-05 16:01:27 -0800

    Add sieve of eratosthenes

--- primes.py
+++ primes.py
@@ -2,6 +2,7 @@
 
 import math
 from functools import reduce
+import pprint
 
 
 def divisors(n):
@@ -27,7 +28,7 @@ def prime_divisors(n):
     
     return p_div
 
-
+################################################################################
 # Every prime > 3 has form 6k-1 or 6k+1
 def is_prime(n):
     if n < 2:
@@ -51,11 +52,21 @@ def is_prime(n):
 
     return True
 
-
-# only keep table of odd integers
+################################################################################
+# Only keep table of odd integers to save space
 # https://www.quora.com/Can-you-write-a-C-program-that-finds-all-prime-numbers-from-2-to-2-billion-in-under-1-second-on-an-average-500-PC
-def sieve_of_eras():
-    pass
+def sieve_of_eratosthenes(n):
+    last = int(math.sqrt(n)) + 1
+    sieve = {k: True for k in range(3, n+1, 2)}
+
+    for i in range(3, last, 2):
+        if sieve[i]:
+            for j in range(i*i, n+1, i):
+                sieve[j] = False
+
+    return [2] + [k for k in range(3, n+1, 2) if sieve[k]]
+
+
 
 """
 https://stackoverflow.com/questions/15347174/python-finding-prime-factors
@@ -71,8 +82,7 @@ if n<=2:
 
 """
 
-
-
+################################################################################
 # Korselt's criterion
 # Theorem (A. Korselt 1899): A positive composite integer n is a Carmichael number if and only if 
 # n is square-free, and for all prime divisors p of n, it is true that (p - 1) | (n - 1).
@@ -120,7 +130,10 @@ print("\nPrime factors of {}: {}".format(n, p_div))
 product = reduce(lambda x, y: x*y, p_div)
 print("Verifying product of prime factors is: {}".format(product))
 
-
+sieve = sieve_of_eratosthenes(1000)
+print("Sieve of Eratosthenes up to n = 1000:")
+#pprint.pprint(sieve)
+print(sieve)
 
 
 
